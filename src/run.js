e4b88d31ad4a40460752579e7088b3e9954d9221
Milan <milanm1120@gmail.com>
2021-12-22 14:17:42 -0500

    build Run class in run.js

---
+++ src/run.js
@@ -0,0 +1,11 @@
+class Run {
+    constructor({id, time, distance, run_type, comments, runner_id, runner}){
+        this.id = id
+        this.time = time
+        this.distance = distance
+        this.run_type = run_type
+        this.comments = comments
+        this.runner_id = runner_id
+        this.runner = runner
+    }
+}
